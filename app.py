34d01572a59ca05540bad8b33ccada709896a1b2
sae2 <lollol0617@gmail.com>
2023-05-31 04:18:50 +0900

    adf

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+import dialog 
+import google.cloud.dialogflow_v2 as dialogflow
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def project_1():
+    return render_template('project_1.html')
+
+@app.route("/chatpage")
+def chatpage():
+    return render_template('chat_page.html')
+
+@app.route("/infopage")
+def infopage():
+    return render_template('info_page.html')
+
+@app.route("/get_answer", methods=['POST'])
+def get_answer():
+    question = request.form.get('question')  # 사용자의 질문을 가져옵니다.
+
+    answer = dialog.get_answer_form_server(question)
+    print(answer)
+
+    return jsonify({'answer': answer})
+
+if __name__ == "__main__":
+    app.run()
